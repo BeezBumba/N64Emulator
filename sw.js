4028e2c0e258c67cd61ffbf81b1308e6697d33a9
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2024-02-19 20:19:04 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,37 @@
-const KEY = 'N64EMU';
+const filesToCache = [
+	"icons",
+	"assets.zip",
+	"github_logo.png",
+	"index.html",
+	"input_controller.js",
+	"manifest.json",
+	"n64wasm.js",
+	"n64wasm.wasm",
+	"romlist.js",
+	"script.js",
+	"settings.js"
+];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(self.skipWaiting());
+const staticCacheName = "N64EMU";
+
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open(staticCacheName)
+		.then(cache => {
+			return cache.addAll(filesToCache);
+		})
+	);
 });
 
-self.addEventListener('message', (event) => {
-    if (event.data.type === 'CACHE_URLS') {
-        event.waitUntil(
-            caches.open(KEY)
-                .then( (cache) => {
-                    return cache.addAll(event.data.payload);
-                })
-        );
-    }
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    (async () => {
-      try {
-       console.log(`[Service Worker] Attempting live fetch: ${e.request.url}`);
-       const response = await fetch(e.request);
-       const cache = await caches.open(KEY);
-       console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-       cache.put(e.request, response.clone());
-       return response;
-      } catch (err) {
-        console.log(`[Service Worker] Attempting to serve resource from cache: ${e.request.url}`);
-        const r = await caches.match(e.request);
-        if (r) {
-          return r;
-        }
-      }
-    })()
-  );
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request)
+		}).catch(error => {
+		})
+	);
 });
