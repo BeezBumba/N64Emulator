563164a05d823e111b041b48cd796f3e92c71a59
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2024-02-23 19:38:08 -0600

    Update register_sw.js

--- register_sw.js
+++ register_sw.js
@@ -1,12 +1,12 @@
 if ("serviceWorker" in navigator)
 				{
-				navigator.serviceWorker.register("sw.js").then(function(registration)
-					{
-					// Registration successful
-					console.log("ServiceWorker registration successful with scope: " + registration.scope);
-					}).catch(function(err)
-					{
-					// Registration failed
-					console.log("ServiceWorker registration failed: " + err);
-					});
-				}
+	navigator.serviceWorker.register("sw.js")
+		.then(function(registration){
+			// Registration successful
+			console.log("ServiceWorker registration successful with scope: " + registration.scope);
+			}).catch(function(err)
+			{
+			// Registration failed
+			console.log("ServiceWorker registration failed: " + err);
+			});
+		}
