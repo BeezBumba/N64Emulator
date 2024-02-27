0f3f0d512b7ead2731efb1e68160a4928f5d67d1
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2024-02-26 18:00:29 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,18 +19,19 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
       try {
-       console.log(`[Service Worker] Attempting live fetch: ${e.request.url}`);
-       const response = await fetch(e.request);
-       const cache = await caches.open(KEY);
-       console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-       cache.put(e.request, response.clone());
-       return response;
-      } catch (err) {
         console.log(`[Service Worker] Attempting to serve resource from cache: ${e.request.url}`);
         const r = await caches.match(e.request);
         if (r) {
           return r;
         }
+        console.log(`[Service Worker] Attempting live fetch: ${e.request.url}`);
+        const response = await fetch(e.request);
+        const cache = await caches.open(KEY);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+      } catch (err) {
+        console.error(`[Service Worker] Fetch failed: ${err}`);
       }
     })()
   );
