a0f593089b2910930b46364c9cf54dcaebe143df
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2024-06-04 15:45:13 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 var N64WASMSETTINGS = {
     CLOUDSAVEURL: "",
-    SHOWADVANCED: false, //set this to true to enable importing of save files (eep, sra, fla)
-    SHOWOPTIONS: false //set this to true to add options for reset and toggling fps
+    SHOWADVANCED: true, //set this to true to enable importing of save files (eep, sra, fla)
+    SHOWOPTIONS: true //set this to true to add options for reset and toggling fps
 }
