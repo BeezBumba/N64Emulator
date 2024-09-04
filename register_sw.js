3b736210fec8ecaa8b71b86711514c1123494e26
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2024-09-03 19:14:39 -0500

    Update register_sw.js

--- register_sw.js
+++ register_sw.js
@@ -13,3 +13,17 @@ if ('serviceWorker' in navigator) {
       })
       .catch((err) => console.log('SW registration FAIL:', err));
 }
+
+// Add the event listener for the update button
+document.getElementById('update-files').addEventListener('click', () => {
+    if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller.postMessage({
+            type: 'UPDATE_CACHE',
+            payload: [
+                location.href,
+                // cache all files that were pulled in: https://stackoverflow.com/a/55543550/1569320
+                ...performance.getEntriesByType('resource').map((r) => r.name)
+            ]
+        });
+    }
+});
