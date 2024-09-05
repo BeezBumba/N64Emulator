6ef4fb911cfed9b4218a37d06a84d4640bfdfb2c
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2024-09-05 06:37:35 -0500

    Update register_sw.js

--- register_sw.js
+++ register_sw.js
@@ -1,19 +1,17 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', { scope: '/' })
-        .then((registration) => {
-            const data = {
-                type: 'CACHE_URLS',
-                payload: [
-                    location.href,
-                    // cache all files that were pulled in: https://stackoverflow.com/a/55543550/1569320
-                    ...performance.getEntriesByType('resource').map((r) => r.name)
-                ]
-            };
-            if (registration.installing) {
-                registration.installing.postMessage(data);
-            }
-        })
-        .catch((err) => console.error('Service Worker registration failed:', err));
+  navigator.serviceWorker.register('sw.js', { scope: '' })
+      .then((registration) => {
+          const data = {
+              type: 'CACHE_URLS',
+              payload: [
+                  location.href,
+                  // cache all files that were pulled in: https://stackoverflow.com/a/55543550/1569320
+                  ...performance.getEntriesByType('resource').map((r) => r.name)
+              ]
+          };
+          registration.installing.postMessage(data);
+      })
+      .catch((err) => console.log('SW registration FAIL:', err));
 }
 
 // Add the event listener for the update button
